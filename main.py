2d56a6cdd7f742e3a47be7329771f866308de42a
Len-168 <dvar4890@gmail.com>
2023-12-25 23:18:54 +0700

    new fucntion

--- main.py
+++ main.py
@@ -4,29 +4,18 @@ import random
 ld = emulator.LDPlayer(ldplayer_dir='F:/LDPlayer/LDPlayer9')
 # print(ld.emulators)
 
-OPENVPN = 0
-ACTIVE = 1
-
 
 def open_emulator(index):
-
     em = ld.emulators[index]
-
     em.start()
     em.wait_to_started()
-
     ld.sort_window()
 
-    em.run_app(package_name='com.namecheap.vpn')
-    em.wait(7)
-    em.tap_to_img(img_path="img/ClickConnect.png", timeout=0, threshold=0.8)
-    em.wait(7)
-    
 
-    i = 0
+
+    i = 4
     while i < 9:
-        
-        RandomScroll = [15,20,25,30,35,40]
+
         RandomLike = [1,2,3,4,5,6,7,8,9,0]
 
         getApp = open('Packge/app.txt', 'r')
@@ -37,23 +26,61 @@ def open_emulator(index):
         em.run_app(package_name=FacebookApp)
         em.wait(10)
 
-        s_num = random.choice(RandomScroll)
-        for scroll in range(s_num):
-            em.swipe(_from=(211, 703), to=(207, 262),duration=random.randint(250,400))      # Scroll
+#scrool
 
-            Like = random.choice(RandomLike)       # Random Like 
-            if  Like == 0 or Like == 5 or Like == 9:
-                em.wait(random.randint(3,5))         # Sleep before like
-                try:
-                    em.tap_to_img(img_path="img/btnLike.png", timeout=0, threshold=0.8)       #Click Image Like
-                    em.wait(random.randint(3,5))         # Sleep after like
-                except:pass
-                
-            else:
-                em.wait(random.randint(3,7)) # if min like sleep
+        # for scroll in range(random.randint(3,4)):
+        #     em.swipe(_from=(211, 703), to=(207, 262),duration=random.randint(150,300))      # Scroll
+        #     Like = random.choice(RandomLike)       # Random Like 
+        #     if  Like == 0 or Like == 5 or Like == 9:
+        #         em.wait(random.randint(3,6))         # Sleep before like
+        #         try:
+        #             em.tap_to_img(img_path="img/btnLike.png", timeout=0, threshold=0.8)       #Click Image Like
+        #             em.wait(random.randint(3,6))         # Sleep after like
+        #         except:pass
+        #     else:
+        #         em.wait(random.randint(3,7)) # if min like sleep
+        # em.back()
+        # em.wait(8)
+
+# Check-in
+
+        try:
+            em.tap_to_img(img_path="img/check_in/1.png", timeout=0, threshold=0.8)
+            em.wait(5)
+
+            em.tap_to_img(img_path="img/check_in/2.png", timeout=0, threshold=0.8)
+            em.wait(7)
+
+            em.tap_to_img(img_path="img/check_in/3.png", timeout=0, threshold=0.8)
+            em.wait(8)
+
+            em.tap((176, 336))
+            em.wait(5)
+
+            try:
+                em.tap_to_img(img_path="img/check_in/4.png", timeout=0, threshold=0.8)
+                em.wait(8)
+
+                em.tap_to_img(img_path="img/check_in/5.png", timeout=0, threshold=0.8)
+                em.wait(8)
+            except:
+                em.tap_to_img(img_path="img/check_in/6.png", timeout=0, threshold=0.8)
+                em.wait(8) 
+            pass
+
+        except:pass
 
 
-indices_to_open = [5,6,7,8,9]
+
+
+
+
+        
+
+
+
+
+indices_to_open = [7]
 
 
 threads = []
--- testlogic.py
+++ testlogic.py
@@ -1,37 +1,45 @@
 
-import emulator
+# import emulator
 
-ld = emulator.LDPlayer(ldplayer_dir='F:/LDPlayer/LDPlayer9')
-
-
-
-em = ld.emulators [8] 
-em.start ()
-em.wait_to_started()
-
-packages = em.list_packages()
-
-print(packages)
-
-
-# import random
-
-# RandomLike = [1,2,3,4,5,6,7,8,9,0]
-
-# Like = random.choice(RandomLike)
-
-# if  Like == 0 or Like == 2 or Like == 5 or Like == 7 or Like == 9:
-#     print("OK"+str(Like))
+# ld = emulator.LDPlayer(ldplayer_dir='F:/LDPlayer/LDPlayer9')
 
 
 
-# i = 0
-# while i < 9:
+# em = ld.emulators [8] 
+# em.start ()
+# em.wait_to_started()
 
-#     getApp = open('Packge/app.txt', 'r')
-#     App = getApp.readlines()
-#     code = App[i]
-#     i += 1
+# packages = em.list_packages()
 
-#     print(code)
+# print(packages)
+
+
+import random
+
+# # RandomLike = [1,2,3,4,5,6,7,8,9,0]
+
+# # Like = random.choice(RandomLike)
+
+# # if  Like == 0 or Like == 2 or Like == 5 or Like == 7 or Like == 9:
+# #     print("OK"+str(Like))
+
+
+
+# # i = 0
+# # while i < 9:
+
+# #     getApp = open('Packge/app.txt', 'r')
+# #     App = getApp.readlines()
+# #     code = App[i]
+# #     i += 1
+
+# #     print(code)
                         
+
+
+for scroll in range(random.randint(20,60)):
+
+    print(scroll)
+
+    if  random.randint(20,60) == 0 or 5 or 9:
+        print("xx")
